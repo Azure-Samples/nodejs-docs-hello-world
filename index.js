2bfc7e6497fa92680f7ae18e7abc524c8efcff7e
Cory Fowler <cfowler@microsoft.com>
2017-03-15 17:31:21 -0700

    initial code check-in

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
