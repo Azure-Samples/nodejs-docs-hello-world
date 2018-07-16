000133255be1f98a5a33e1d19ec075c039866d65
milindvb <milind141@yahoo.com>
2018-07-16 11:56:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const app = express()
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
+    app.get('/', function (req, res) {
+       res.send(process.version)
+    })
     response.end("Hello World!");
 
 });
 
 var port = process.env.PORT || 1337;
 server.listen(port);
-app.get('/', function (req, res) {
-    res.send(process.version)
-  })
-
 
 console.log("Server running at http://localhost:%d", port);
