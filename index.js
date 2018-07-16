aed1a0bd1a8ebc7e57a69c8a98b9a24c1ba2572a
milindvb <milind141@yahoo.com>
2018-07-16 17:22:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
         const version = process.version
         response.write("nodejs version: ");
-        response.write(veriion);
+        response.write(version);
         response.end("");
 
 });
