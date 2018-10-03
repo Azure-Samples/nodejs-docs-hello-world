86761312b5a159c8d366c8e11e239982b73da789
Norem80 <norem80tv@gmail.com>
2018-10-03 22:01:01 +0200

    Changed code to new ES6 syntax
    I've changed var to const and used a template string insted of old string syntax also deleted semicolons beacause they are unnecessary in javascript

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var http = require('http');
+const http = require('http')
 
-var server = http.createServer(function(request, response) {
+const server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, {"Content-Type": "text/plain"})
+    response.end("Hello World!")
 
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+const port = process.env.PORT || 1337
+server.listen(port)
 
-console.log("Server running at http://localhost:%d", port);
+console.log(`Server running at http://localhost:${port}`)
