1f6c9e21f6e51343045cde1b2908fa2fce7a18b0
Norem80 <norem80tv@gmail.com>
2018-10-03 22:05:45 +0200

    Callback functions
    also added a callback function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http')
 
-const server = http.createServer(function(request, response) {
+const server = http.createServer((request, response) => {
 
     response.writeHead(200, {"Content-Type": "text/plain"})
     response.end("Hello World!")
