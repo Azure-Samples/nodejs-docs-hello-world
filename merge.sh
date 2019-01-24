486c141fb6ad409e1cee48f09a6de31600ff3247
G33tha <geetha.p@optit.co>
2019-01-24 10:05:35 +0530

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+http://52.172.131.47:3000/status?stageid=stage2
 git checkout master
 git merge --no-ff $CIRCLE_BRANCH
 git config --global push.default matching
 git push https://$CIRCLE_PROJECT_USERNAME:$PASSWORD@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME
+http://52.172.131.47:3000/status?stageid=stage2
 
