9210a21d5682c12f9e327da3b8a88145cc3d1d5f
G33tha <geetha.p@optit.co>
2019-01-24 10:58:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 node index.js &
 #if [ $? != 0 ] ;then
-   curl http://52.172.131.47:3000/status?stageId=stage2&val=1 
+curl http://52.172.131.47:3000/status?stageId=stage2&val=1 
 #else
    #curl http://52.172.131.47:3000/status?stageId=stage2&val=0
 #fi
