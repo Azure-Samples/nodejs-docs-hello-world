4c5182003d959e4175f91853339b1c11c4c5e571
milindvb <milind141@yahoo.com>
2019-05-03 17:13:39 -0500

    Update index.js
    adding yargs

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
         const version = process.version;
         var pid = process.pid;
-        var output = "nodejs version = "+ process.version + ", Process Id = " + pid;
+        const argv = require('yargs').argv
+
+        var output = "nodejs version = "+ process.version + ", Process Id = " + pid + ", mongourl = "+ argv.mongourl;
         response.write(output);
-        response.end("------");
+        response.end("------------");
 });
 
 var port = process.env.PORT || 8081;
