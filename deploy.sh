c21da9d5f115a1e03e7efdc9acc385c212ed0926
milindvb <milind141@yahoo.com>
2019-05-07 14:00:52 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -112,7 +112,7 @@ selectNodeVersion
 # 3. Install NPM packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  NPM_CMD="/opt/nodejs/10.1.0/bin/npm"
+  NPM_CMD="/opt/nodejs/10.14.1/bin/npm"
   echo "performing npm install"
   echo "versions used : "
   npm -v
