e7cb7fdbdbb3b82832b19e510366897f58fe72c8
milindvb <milind141@yahoo.com>
2019-05-23 16:01:14 -0500

    updating setheader

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+var http = require('http');
 const express = require('express')
-
 const app = express()
- 
 
-var port = process.env.port || 8081;
- 
 
-app.get('/', function (req, res) {
+var server = http.createServer(function(request, response) {
 
- res.setHeader('Transfer-Encoding', 'chunked');
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.setHeader('Transfer-Encoding', 'chunked');
+    sponse.useChunkedEncodingByDefault = true;     const version = process.version;
+        var pid = process.pid;
+        //const argv = require('yargs').argv
 
- res.useChunkedEncodingByDefault = true;
+        //var output = "nodejs version = "+ process.version + ", Process Id = " + pid + ", mongourl = "+ argv.mongourl;
+        var output = "nodejs version = "+ process.version + ", Process Id = " + pid;
+        response.write(output);
+        response.end("---dev2---------");
+});
 
- res.send(process.version)
+var port = process.env.PORT || 8081;
+server.listen(port);
 
- });
-
-app.listen(port, () => console.log('Example app listening on port 8081!'))
+console.log("Server running at http://localhost:%d", port);
