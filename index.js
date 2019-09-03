41fb61a1fe8e1ac62b4d861e9689cbd041c38017
David Matousek <dmatousek@jhancock.com>
2019-09-03 09:50:40 -0400

    text change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello KEVIN! GitHub Actions is cool!");
+    response.end("Hello Oliver! GitHub Actions is cool!");
 });
 
 const port = process.env.PORT || 1337;
