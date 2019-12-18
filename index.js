df6078f9ece3d633be65a372255fd644cfbe3b75
milindvb <mibelhe@microsoft.com>
2019-12-18 09:59:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,37 @@ const express = require('express')
 const app = express()
 var fs = require('fs');
 var path = require('path');
+const Nightmare = require('nightmare');
+const path = require('path');
+const nightmare = Nightmare({
+    show: false,
+    gotoTimeout: 120000,
+    waitTimeout: 120000,
+  });
+const pathToPdfFile = path.resolve('test.pdf');
 
 
 var server = http.createServer(function(request, response) {
-    response.setHeader('Connection', 'Transfer-Encoding');
-    response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    response.setHeader('Transfer-Encoding', 'chunked');
-    response.useChunkedEncodingByDefault = true;
-    //response.writeHead(200, {"Content-Type": "text/plain"});
-    response.writeHead(200);
-    
+  
 
         var pid = process.pid;
         //const argv = require('yargs').argv
 
         //var output = "nodejs version = "+ process.version + ", Process Id = " + pid + ", mongourl = "+ argv.mongourl;
         var output = "nodejs version = "+ process.version + ", Process Id = " + pid;
-        fs.createReadStream(path.resolve(__dirname, 'sample23kb.txt')).pipe(response);
-        //response.write(output);
+        response.write(output);
         //response.end("---dev2---------");
+        nightmare
+           .goto(`https://www.yahoo.com`)
+          //.wait('.nightmare-loaded-indicator')
+          .pdf(pathToPdfFile)
+          .end()
+          .then(() => {
+          console.log(pathToPdfFile);
+        })
+        .catch((error) => {
+           console.error('Render failed: ', error)
+        });
 });
 
 var port = process.env.PORT || 8081;
