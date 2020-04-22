c59ae47195c1da5c8514d7e23f4b3ecb066263ff
Marius-Cătălin Bădinescu <383778@student.fontys.nl>
2020-04-22 18:39:51 +0200

    Revert "test"
    This reverts commit 239924377181627dde91f1e02d4b0cd5c17c4365.

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
+// });
+
+// const port = process.env.PORT || 1337;
+// server.listen(port);
+
+    // console.log("Server running at http://localhost:%d", port);
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mySqlConnection = require('./connection');
+const ProductsRouters = require("./routers/products");
+let app = express();
+
+app.use(bodyParser.json());
+
+app.use("/",ProductsRouters);
 
 const port = process.env.PORT || 1337;
-server.listen(port);
+app.listen(port, ()=> console.log("Express server running on port:3000"));
 
 console.log("Server running at http://localhost:%d", port);
-
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mySqlConnection = require('./connection');
-// const ProductsRouters = require("./routers/products");
-// let app = express();
-
-// app.use(bodyParser.json());
-
-// app.use("/",ProductsRouters);
-
-// app.listen(3000, ()=> console.log("Express server running on port:3000"));
-
