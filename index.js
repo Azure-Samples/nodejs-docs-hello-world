239924377181627dde91f1e02d4b0cd5c17c4365
Marius-Cătălin Bădinescu <383778@student.fontys.nl>
2020-04-22 18:36:00 +0200

    test

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((request, response) => {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
-// });
-
-// const port = process.env.PORT || 1337;
-// server.listen(port);
-
-    // console.log("Server running at http://localhost:%d", port);
-
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const mySqlConnection = require('./connection');
-const ProductsRouters = require("./routers/products");
-let app = express();
-
-app.use(bodyParser.json());
-
-app.use("/",ProductsRouters);
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+});
 
 const port = process.env.PORT || 1337;
-app.listen(port, ()=> console.log("Express server running on port:3000"));
+server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mySqlConnection = require('./connection');
+// const ProductsRouters = require("./routers/products");
+// let app = express();
+
+// app.use(bodyParser.json());
+
+// app.use("/",ProductsRouters);
+
+// app.listen(3000, ()=> console.log("Express server running on port:3000"));
+
