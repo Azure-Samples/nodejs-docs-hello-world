3638d143589339a80cc0892b550471007e9a2209
Shaun Laughland <shaun.laughland@hrsdc-rhdcc.gc.ca>
2020-04-24 14:35:27 -0400

    fix index test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
