a8914c3c80baebfb1e63917fa06bc3faa5bab962
Shaun Laughland <shaun.laughland@hrsdc-rhdcc.gc.ca>
2020-04-27 13:31:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World from Jenkins deploy!");
+    response.end("Hello World from Jenkins deploy! I'm using my own scripts!");
 });
 
 const port = process.env.PORT || 1337;
