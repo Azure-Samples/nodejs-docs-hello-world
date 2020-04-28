ef20194df867141b1fdb24b282930bf51649e583
auamod <51243871+auamod@users.noreply.github.com>
2020-04-28 10:44:32 +0200

    check the body of /register and /login for proper values

--- routers/auth.js
+++ routers/auth.js
@@ -7,21 +7,29 @@ const jwt = require('jsonwebtoken')
 let secret = "secret"
 
 Router.post('/register', async (req, res) => {
+        if (!req.body.name){
+                return res.status(400).send("Name missing")
+        }
+
+        if (!req.body.email){
+                return res.status(400).send("Email missing")
+        }
+
+        if (!req.body.password){
+                return res.status(400).send("Password missing")
+        } 
+        
         let name = req.body.name
         let email = req.body.email
         let password = req.body.password
-        if (ValidateEmail(email)) {
-                
-                let hashedPassword = await bcrypt.hash(password, 10)
 
-                console.log("/register....Email is valid")
-                console.log("/register....Password: " + password)
-                console.log("/register....HashedPassword: " + hashedPassword)
+        if (ValidateEmail(email)) {                
+                let hashedPassword = await bcrypt.hash(password, 10)
 
                 inhouseDB.query(`select * from inhouse.user where email = "${email}"`, (err, rows, fields) => {
                         if (!err) {
                                 if (rows[0]) {
-                                        return res.status(400).send("The email already exists")
+                                        return res.status(400).send("Email already exists")
                                 } else {
                                         ///Create account
                                         inhouseDB.query(
@@ -41,13 +49,21 @@ Router.post('/register', async (req, res) => {
                         }
                 })
         } else {
-                console.log("/register....Email is not valid")
-                return res.status(400).send("The email is not valid")
+                console.log("/register....Email is not valid: " + email)
+                return res.status(400).send("Email is not valid")
         }
 })
 
 
 Router.post('/login', async (req, res) => {
+        if (!req.body.email){
+                return res.status(400).send("Email missing")
+        }
+
+        if (!req.body.password){
+                return res.status(400).send("Password missing")
+        }
+        
         let email = req.body.email
         let password = req.body.password
 
@@ -58,24 +74,24 @@ Router.post('/login', async (req, res) => {
                                 bcrypt.compare(password, rows[0].password, function (err, isMatch) {
                                         if (err) {
                                                 console.log("/login....bcrypt-Error: " + err)
-                                                res.status(500).send("Internal Server Error")
+                                                return res.status(500).send("Internal Server Error")
                                         }
 
                                         if (isMatch) {
                                                 //TODO Send JWT Token
-                                                res.status(200).send("OK")
+                                                return res.status(200).send("OK")
                                         } else {
-                                                res.status(400).send("Password is not correct")
+                                                return res.status(400).send("Password is not correct")
                                         }
                                 })
                         } else {
                                 //Email does not exist
-                                res.status(400).send("The email does not exist")
+                                return res.status(400).send("The email does not exist")
                         }
 
                 } else {
                         console.log("/login....DB_SELECT-error: " + err)
-                        res.status(500).send("Internal Server Error")
+                        return res.status(500).send("Internal Server Error")
                 }
         })
         // let user = inhouseDB.query(`select email from inhouse.users where email = "${req.body.email}"`, (err, rows, fields) => {
