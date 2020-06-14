2ebde249d866984eb643db5c6970ad78d08bfe47
rjeeva <jeeva@klenty.com>
2020-06-15 00:41:30 +0530

    Add hello name feature

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require('http');
+var url = require('url');
 
 const server = http.createServer((request, response) => {
+    var queryData = url.parse(request.url, true).query;
+    if (queryData.name) {
+        // user told us their name in the GET request, ex: http://host:8000/?name=Tom
+        response.end('Hello ' + queryData.name + '\n');
+    }
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 });
