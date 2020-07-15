07e6d9eac4ff25fd760925ff7ce272bc442a73a8
Elies06 <40525678+Elies06@users.noreply.github.com>
2020-07-15 22:22:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Salut Elies");
+    response.end(" j'ai dit !! Salut Elies");
 });
 
 const port = process.env.PORT || 1337;
