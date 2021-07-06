31314188e0a7178f06c2f44648201bbc2a4dc77f
aravind-shiva <84741977+aravind-shiva@users.noreply.github.com>
2021-07-06 12:50:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "text/plain": "colour/blue"});
     response.end("Hello World!");
 });
 
