9188b4a2228f9a1ff10d2bfc52930b89525084fb
Gene Laisne <glaisne@users.noreply.github.com>
2022-05-05 15:41:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
+    response.end("Hello World!");
 });
 
 const port = process.env.PORT || 1337;
