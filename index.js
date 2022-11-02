0bb5a712b051c9bc6fb23bde24c5eaf106e25048
Julia Kulla-Mader <julia.kullamader@microsoft.com>
2022-11-02 17:43:46 -0400

    Small update to match updated Node.js example.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
