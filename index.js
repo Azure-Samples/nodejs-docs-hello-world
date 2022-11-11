31afb07174861a5a676add0b65d65e3c3ac09593
Hifumi Takai <62784012+thanaism@users.noreply.github.com>
2022-11-11 16:16:56 +0900

    Fix: listen port
    App Service port number depends on environment variables.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
