a43e0c4ce461e8cf45e6f45f90654147dd704e02
majekal1 <104643397+majekal1@users.noreply.github.com>
2022-12-08 10:07:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 // https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
 // ***************************************************************************
 
-response.end("Hello World!");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const crypto = require('crypto');
 const pkg = require('./package.json');
+response.end("Hello World!");
 
 
 // App constants
